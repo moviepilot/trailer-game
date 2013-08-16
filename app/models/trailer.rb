37be5fc9980a7c0d6bab65ef3dd24cdeb423bbc6
Michal Taberski <m.taberski@gmail.com>
2013-08-16 13:07:10 +0200

    attraccessible

--- app/models/trailer.rb
+++ app/models/trailer.rb
@@ -1,5 +1,6 @@
 class Trailer < ActiveRecord::Base
-  attr_accessible :category_id, :title, :youtube_link
+
+  attr_accessible :category_id, :title, :youtube_link, :answer0, :answer1, :answer2, :answer3, :correct_answer
 
   belongs_to :category
 
