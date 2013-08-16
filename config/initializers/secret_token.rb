5aee839707bb7afc8749be0968cc6c2812cef140
Michal Taberski <m.taberski@gmail.com>
2013-08-16 11:08:18 +0200

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrailerGame::Application.config.secret_token = 'c5679707ee8767930017a8542d1727621ccbadf1c7bab2e60a3e534a7883f65adb0ee725f3484b7e60cd91068d3fb2391fa45f988497443b77e9de6c4daecf90'
